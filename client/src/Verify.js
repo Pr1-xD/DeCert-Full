724e3320edd391768c03f19ee1b83a53a8784020
Pr1-xD <63440566+Pr1-xD@users.noreply.github.com>
2022-05-21 10:51:01 +0530

    QR Code Added

--- client/src/Verify.js
+++ client/src/Verify.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
 import { useParams } from "react-router-dom";
+import axios from 'axios' 
+import QRCode from "react-qr-code";
 
 function Verify({accounts,setAccounts}) {
     let params = useParams()
     let serialNo=params.SerialNumber
     const [ipfsLink,setIpfsLink]=useState(null)
-   
+
+    axios.get('http://localhost:5000/verify/'+params.SerialNumber)
+    .then(res=>{
+      console.log(res)
+    })
+
+
 
 
     return (
         <div>
             Verify:{params.SerialNumber}
+            <QRCode value={"http://localhost:3000/"+params.SerialNumber} />
         </div>
     );
 }
