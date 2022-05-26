ea30876c1cf9ad90df5587b068387e108e37b6e2
Pr1-xD <63440566+Pr1-xD@users.noreply.github.com>
2022-05-26 15:26:44 +0530

    Added Verification on Frontend

--- client/src/Verify.js
+++ client/src/Verify.js
@@ -8,18 +8,46 @@ function Verify({accounts,setAccounts}) {
     let serialNo=params.SerialNumber
     const [ipfsLink,setIpfsLink]=useState(null)
 
-    axios.get('http://localhost:5000/verify/'+params.SerialNumber)
+    function verifySerial(){
+        axios.get('http://localhost:5000/verify/'+params.SerialNumber)
     .then(res=>{
       console.log(res)
+      console.log(res.data.IPFSUrl)
+      setIpfsLink(res.data.IPFSUrl)
     })
+    }
+    function redirectToIPFS(){
+        window.location.href=ipfsLink
+    }
 
 
 
 
     return (
-        <div>
-            Verify:{params.SerialNumber}
+        <div class="container">
+            <br/>
+            <br/>
+            <br/>
+            <div class="row">
+            <div class="col-sm align-self-center font-weight-bold">
+            <h4>Serial Number: {params.SerialNumber}</h4>
+            </div>
+            <div class="col-sm">
+            <h4>QR Code: </h4>
             <QRCode value={"http://localhost:3000/"+params.SerialNumber} />
+            </div>
+            <div class="col-sm align-self-center">
+            <button class="btn btn-primary" onClick={verifySerial}>Verify</button>
+            </div>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <div class="text-center">
+                {ipfsLink?<h3>Result: </h3>:<></>}
+                {ipfsLink?<h3>Verified</h3>:<></>}
+                {ipfsLink?<h3 onClick={redirectToIPFS}>Click to View</h3>:<></>}
+            </div>
         </div>
     );
 }
